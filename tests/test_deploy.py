d94d7680f0698bbfb1905d8feac68e23edf495b7
Thomas Fan <thomasjpfan@gmail.com>
2018-01-24 14:50:05 -0500

    TST: Fix

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -1,15 +1,7 @@
-import os
-
-
 def test_users_exists(host):
     u = host.user("deploy")
     assert u.exists
     assert "docker" in u.groups
-
-
-def test_local_ssh_dir_exists(host):
-    assert os.path.exists("tests/ssh_keys/private/localhost.pem")
-    assert os.path.exists("tests/ssh_keys/public/localhost.pub")
 
 
 def test_authorized_keys(host):
@@ -18,10 +10,7 @@ def test_authorized_keys(host):
     assert file.is_file
     content = file.content_string
 
-    ssh_key_dir = "tests/ssh_keys"
-    assert os.path.exists(ssh_key_dir)
-
-    pub_key = os.path.join(ssh_key_dir, "public", "localhost.pub")
+    pub_key = "tests/id_deploy.pub"
 
     with open(pub_key, "r") as f:
         pub_content = f.read()
