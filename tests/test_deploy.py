3c60e271aa3fb143809a8dd7ce672dd807edd5d2
Thomas Fan <thomasjpfan@gmail.com>
2018-01-24 15:41:26 -0500

    ENH: Add to sudoers

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -16,3 +16,13 @@ def test_authorized_keys(host):
         pub_content = f.read()
 
     assert pub_content in content
+
+
+def test_user_in_sudoers(host):
+    file = host.file("/etc/sudoers")
+    assert file.exists
+    assert file.is_file
+    content = file.content_string
+
+    assert "deploy ALL=(ALL) NOPASSWD: ALL" in content
+
